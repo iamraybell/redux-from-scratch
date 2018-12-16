b034deb78bce31da2a0ba1250a90979dc8751238
Ray Bell <iamraybell@gmail.com>
2018-12-16 00:06:35 -0800

    added more actions

--- index.js
+++ index.js
@@ -1,43 +1,72 @@
-//this is our reducer.  this is a pure function.
+const createStore = require('./store');
+
 function todos(state = [], action){
+
     switch (action.type){
         case 'ADD_TODO':
-            return state.concat([action.todo])
+            return state.concat([action.payload])
+        case 'REMOVE_TODO':
+            return state.filter((todo)=>{
+                return todo.id !== action.payload.id;
+            })
+        case 'UPDATE_TODO':
+            return state.map((todo)=>{
+                return (todo.id === action.payload.id) ? 
+                {...action.payload}: 
+                todo;
+        })
         default:
             return state;
     }
 };
 
 //this creates are store.
-function createStore () {
-    let state;
-    let listeners = [];
-    const getState = () => state;
-    const subscribe = (listener) => {
-        listeners.push(listener);
-        return ()=>{
-            listeners = listeners.filter(curItem =>{
-                curItem !== listener;
-            })
-        }
-    }
 
-     const dispatch = (action) => {
-        state = reducer(state, action);
 
-        listeners.forEach((listener)=>{
-            listener();
-        })
-     }
-     
-    return {
-        getState,
-        subscribe,
-        dispatch,
-    }
-}
-
-const store = createStore();
+const store = createStore(todos);
 let unSub1 =  store.subscribe(()=>{
     console.log('im sub1');
 });
+
+store.dispatch({
+    type: 'ADD_TODO',
+    payload:{
+        id:0,
+        name:'read a book',
+        complete: false,
+    }
+});
+
+store.dispatch({
+    type: 'ADD_TODO',
+    payload:{
+        id:1,
+        name:'sleep',
+        complete: true,
+    }
+});
+
+store.dispatch({
+    type: 'ADD_TODO',
+    payload:{
+        id:2,
+        name:'cry',
+        complete: true,
+    }
+});
+
+store.dispatch({
+    type: 'REMOVE_TODO',
+    payload:{
+        id:1,
+    }
+});
+
+store.dispatch({
+    type: 'UPDATE_TODO',
+    payload:{
+        id:2,
+        name:'Be happy',
+        complete: false,
+    }
+})
---
+++ store.js
@@ -0,0 +1,28 @@
+const createStore = (reducer)=>{
+    let state;
+    let listeners = [];
+    const getState = () => state;
+    const subscribe = (listener) => {
+        listeners.push(listener);
+        return ()=>{
+            listeners = listeners.filter(curItem =>{
+                curItem !== listener;
+            })
+        }
+    }
+
+     const dispatch = (action) => {
+        state = reducer(state, action);
+        console.log(state)
+        listeners.forEach((listener)=>{
+            listener();
+        })
+     }
+     
+    return {
+        getState,
+        subscribe,
+        dispatch,
+    }
+}
+module.exports = createStore;
